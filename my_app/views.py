dc93cb4c58e0c43f99bdc801e764e031176974bb
geraldinyiama <geraldinyiama@gmail.com>
2020-09-25 10:52:27 +0100

    added the jumia route to scrap

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,10 @@
+import requests
+from urllib.parse import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
-import requests
+from . import models
 
-# Create your views here.
+BASE_TEST_URL = 'https://www.jumia.com.ng/catalog/?q={}'
 
 
 def home(request):
@@ -11,8 +13,26 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_TEST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'core'})
+
+    final_posting = []
+    for post in post_titles:
+        post_title = post.find(class_='name').text
+        post_image = post.find(class_='img').get('data-src')
+        post_url = post.get('href')
+        if post.find(class_='prc'):
+            post_price = post.find(class_='prc').text
+        else:
+            post_price = post.find(class_='prc').text
+        final_posting.append((post_title, post_price, post_image, post_url))
+
     stuff_for_frontend = {
-        'search': search
+        'search': search,
+        'final_posting': final_posting
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
